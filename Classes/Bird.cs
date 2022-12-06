6a99272969efc1f59109f4deb546b1dca805729d
ArtemNY42 <109698820+ArtemNY42@users.noreply.github.com>
2022-12-06 19:43:53 +0300

    Created test class

---
+++ Classes/Bird.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataStructures.Classes
+{
+    public class Bird
+    {
+        public void Fly(){
+            Console.WriteLine("Flying!");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DataStructures.Classes;
+
+namespace DataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Bird a = new Bird();
+            a.Fly();
+        }
+    }
+}
