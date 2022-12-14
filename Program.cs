9f9ce14cf1304735220571bea63c65d49a39c9b0
ArtemNY42 <109698820+ArtemNY42@users.noreply.github.com>
2022-12-14 19:15:57 +0300

    Added LinkedList Class

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace DataStructure
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Test Linked List
+            DataStructures.Classes.LinkedList<int> linkedList = new DataStructures.Classes.LinkedList<int>();
+            linkedList.Add(1);
+            linkedList.Add(2);
+            linkedList.Add(3);
+            linkedList.Add(4);
+            foreach(int current in linkedList){
+                Console.WriteLine(current);
+            }
+            linkedList.Delete(2);
+            Console.WriteLine();
+            foreach(int current in linkedList){
+                Console.WriteLine(current);
+            }
         }
     }
 }
